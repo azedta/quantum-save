8400abe9346b8cf61775c72b682c88488e69d4ae
Aziz Tazrout <aztazrout@gmail.com>
2025-11-05 08:48:46 -0500

    Initial Commit

---
+++ src/main/java/com/quantumsave/quantum_save/QuantumSaveApplication.java
@@ -0,0 +1,13 @@
+package com.quantumsave.quantum_save;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumSaveApplication.class, args);
+	}
+
+}
